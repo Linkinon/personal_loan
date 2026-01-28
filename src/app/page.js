5914b01639563c75be3740e433a0aacd421db0fc
Prakhar Pratap Singh <adops@linkinon.com>
2026-01-28 15:11:45 +0530

    text changes

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ const PersonalLoanPage = () => {
       <div className="max-w-4xl mx-auto px-4 sm:px-6 py-3">
         
         {/* Top Note */}
-        <div className="text-center text-[7px] md:text-[10px]  text-red-700 font-semibold mb-2">
+        <div className="text-center text-[7px] md:text-[10px]  text-red-700  mb-2">
           This page is a paid advertisement and referral service for personal loan options.
           It is not a government website.
         </div>
@@ -115,7 +115,7 @@ const PersonalLoanPage = () => {
               <p><strong>Step 3:</strong> Review available options and decide whether moving forward makes sense.</p>
             </div>
 
-            <div className="bg-red-50 border border-red-200 rounded-lg p-3 text-xs  text-red-900 mt-4 max-w-4xl mx-auto">
+            <div className="bg-red-50 border border-red-200 rounded-lg pl-4 py-2 text-[10px]  text-red-900 mt-4 max-w-4xl mx-auto">
               You’re not applying for a loan on this page.
               Reviewing options does not require you to accept an offer or move forward.
             </div>
@@ -130,8 +130,8 @@ const PersonalLoanPage = () => {
         </div>
 
         {/* Footer */}
-        <footer className="mt-6 text-xs sm:text-sm text-gray-600 text-center space-y-3 px-2">
-          <div className="flex flex-wrap justify-center gap-2 text-xs">
+        <footer className=" text-xs sm:text-sm text-gray-600 text-center space-y-4 px-2">
+          <div className="flex flex-wrap justify-center gap-2 text-[8px]">
             <Link href="/privacy" className="underline text-gray-900">Privacy Policy</Link>
             <span>|</span>
             <Link href="/terms" className="underline text-gray-900">Terms of Use</Link>
